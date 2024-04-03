3eead3c30ba3004f66d5936c4484f6498ddaf8f1
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-04-04 04:59:10 +0600

    right side section added

--- src/Pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa";
+import qZone1 from "../../../assets/qZone1.png"
+import qZone2 from "../../../assets/qZone2.png"
+import qZone3 from "../../../assets/qZone3.png"
+
+
 
 const RightSideNav = () => {
       return (
             <div>
-                  <h1 className="text-xl">Right Side Nav</h1>
+                  <div className="p-4 ">
+                        <h2 className="text-3xl">Login with</h2>
+                        <button className="btn btn-outline w-full my-4"><FaGoogle/> Login with Google</button>
+                        <button className="btn btn-outline w-full my-4"><FaGithub/> Login with Github</button>
+                  </div>      
+                  <div className="p-4">
+                        <h2 className="text-3xl">Find Us</h2>
+                        <a className="flex gap-4 items-center border p-4 text-lg rounded-t-lg" href="">
+                              <FaFacebook/> Facebook
+                        </a>
+                        <a className="flex gap-4 items-center border p-4 text-lg" href="">
+                              <FaTwitter/> Twitter
+                        </a>
+                        <a className="flex gap-4 items-center border p-4 text-lg rounded-b-lg" href="">
+                              <FaInstagram/> Instagram
+                        </a>
+                  </div>  
+                  
+                  <div className="p-4 ">
+                        <h2 className="text-3xl">Q-Zone</h2>
+                        <img src={qZone1} alt="" />
+                        <img src={qZone2} alt="" />
+                        <img src={qZone3} alt="" />
+                        
+                  </div>
             </div>
       );
 };
